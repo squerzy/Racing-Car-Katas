6574a2b88d4cedf57e74044378d7a40a6976e65f
Jerzy Kirchner <jerzy.kirchner@squer.io>
2023-03-29 19:37:23 +0200

    testsensor implemented

--- go/tire-pressure-monitoring-system/alarm_test.go
+++ go/tire-pressure-monitoring-system/alarm_test.go
@@ -10,14 +10,40 @@ type alarmTest struct {
 	actual   func() bool
 }
 
-func TestNoAlarmTriggered(t *testing.T) {
+type TestSensor struct {
+	nextVal int
+}
+
+func (t TestSensor) popNextPressurePsiValue() int {
+	return 20
+}
+
+func TestNoAlarmMidrange(t *testing.T) {
 	t.Run("Do something", func(t *testing.T) {
-		a := NewAlarm(nil)
+
+		s := TestSensor{
+			nextVal: 20,
+		}
+		a := NewAlarm(s)
 		a.check()
 
 		if a.isAlarmOn() {
 			t.Error("Expected no alarm but was Alarm")
 		}
 	})
+}
 
+func TestAlarmBelowRange(t *testing.T) {
+	t.Run("Do something", func(t *testing.T) {
+
+		s := TestSensor{
+			nextVal: 10,
+		}
+		a := NewAlarm(s)
+		a.check()
+
+		if !a.isAlarmOn() {
+			t.Error("Expected alarm did not get one")
+		}
+	})
 }
